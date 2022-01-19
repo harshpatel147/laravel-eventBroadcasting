0e410e89e71eb0e7a184052721dac210c7323848
Harsh <harsh.microtechos@gmail.com>
2022-01-19 17:36:18 +0530

    user's details display issuse fix

--- routes/channels.php
+++ routes/channels.php
@@ -18,6 +18,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('message.{id}', function ($user, $id) {
-    // return (int) $user->id === (int) $id;
-    return true;
+    return (int) $user->id === (int) $id;
+    // return true;
 });
